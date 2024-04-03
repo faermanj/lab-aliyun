4ab8b2f418a43eb05dad76dd923db985fb73e677
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-04-03 13:55:02 +0000

    wip

--- ros-create-stack.sh
+++ ros-create-stack.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+set -ex
 
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
 AY_REGION="eu-central-1"
@@ -33,27 +33,57 @@ aliyun oss cp "$DIR/infra/ros/template.ros.yaml" $AY_OBJECT_KEY --region $AY_REG
 aliyun oss set-acl $AY_OBJECT_KEY public-read --region $AY_REGION
 
 AY_TEMPLATE_URL="https://$AY_BUCKET_NAME.oss-$AY_REGION.aliyuncs.com/$AY_OBJECT_NAME"
-AY_STACK_NAME="ay-${USER}-4"
+AY_STACK_NAME="ay-${USER}-$(date +%s)"
+
+AY_SSH_KEY=$(cat ~/.ssh/id_rsa.pub)
+
+echo "**** Creating Stack $AY_STACK_NAME with template $AY_TEMPLATE_URL"
 
 aliyun ros CreateStack --region $AY_REGION \
     --StackName $AY_STACK_NAME \
-    --Parameters.0.ParameterKey=RegionId \
-    --Parameters.0.ParameterValue=$AY_REGION \
-    --Parameters.1.ParameterKey=ZoneAId \
-    --Parameters.1.ParameterValue=$AY_ZONE_A \
-    --Parameters.2.ParameterKey=ZoneBId \
-    --Parameters.2.ParameterValue=$AY_ZONE_B \
-    --TemplateURL $AY_OBJECT_KEY | tee .ay-stack-create.out.log
+    --Parameters.1.ParameterKey="RegionId" \
+    --Parameters.1.ParameterValue="$AY_REGION" \
+    --Parameters.2.ParameterKey="ZoneAId" \
+    --Parameters.2.ParameterValue="$AY_ZONE_A" \
+    --Parameters.3.ParameterKey="ZoneBId" \
+    --Parameters.3.ParameterValue="$AY_ZONE_B" \
+    --Parameters.4.ParameterKey="PublicKeyBody" \
+    --Parameters.4.ParameterValue="$AY_SSH_KEY" \
+    --Parameters.5.ParameterKey="EnvId" \
+    --Parameters.5.ParameterValue="$AY_STACK_NAME" \
+    --TemplateURL $AY_OBJECT_KEY | tee .ay-stack-create.json
 
-if [ $? -eq 0 ]; then
-    AY_STACK_ID=$(jq -r '.StackId' .ay-stack-create.log)
-    echo "Stack [$AY_STACK_ID] created successfully."
-    echo $AY_STACK_ID > .ay-stack-id.txt
-    aliyun ros GetStack --region $AY_REGION \
+AY_STACK_ID=$(jq -r '.StackId' .ay-stack-create.json)
+echo $AY_STACK_ID > .ay-stack-id.txt
+
+if [ ! -z "$AY_STACK_ID" ]; then
+  echo "Stack $AY_STACK_NAME created successfully with ID $AY_STACK_ID."
+  aliyun ros GetStack --region $AY_REGION \
     --StackId $AY_STACK_ID
 else
-    echo "Failed to create stack $AY_STACK_NAME."
+  echo "Failed to create stack $AY_STACK_NAME."
+  exit 1
 fi
 
+while true; do
+  AY_STACK_STATUS=$(aliyun ros GetStack --region $AY_REGION --StackId $AY_STACK_ID | jq -r '.Stack.Status')
+  if [ "$AY_STACK_STATUS" == "CREATE_COMPLETE" ]; then
+    echo "Stack $AY_STACK_NAME created successfully."
+    break
+  elif [ "$AY_STACK_STATUS" == "CREATE_FAILED" ]; then
+    echo "Stack $AY_STACK_NAME creation failed."
+    break
+  else
+    echo "Stack $AY_STACK_NAME status: $AY_STACK_STATUS"
+    sleep 10
+  fi
+done
+
+# get InstallerInstancePublicIp from stack
+AY_INSTALLER_INSTANCE_PUBLIC_IP=$(aliyun ros GetStack --region $AY_REGION --StackId $AY_STACK_ID | jq -r '.Outputs[] | select(.OutputKey == "InstallerInstancePublicIp") | .OutputValue')
+echo "*******"
+echo "Stack Id: $AY_STACK_ID"
+echo "Installer SSH: ssh root@$AY_INSTALLER_INSTANCE_PUBLIC_IP"
+echo "*******"
 
 echo done
