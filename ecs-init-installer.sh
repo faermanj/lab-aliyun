e6ac9ccf71ff82310408a891b137548f2f0f9994
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-04-10 08:33:20 +0000

    wip

--- ecs-init-installer.sh
+++ ecs-init-installer.sh
@@ -46,5 +46,27 @@ tar zxvf '/tmp/oc/openshift-client-linux.tar.gz' -C '/tmp/oc'
 sudo mv '/tmp/oc/oc' '/usr/local/bin/' 
 sudo mv '/tmp/oc/kubectl' '/usr/local/bin/' 
 rm '/tmp/oc/openshift-client-linux.tar.gz' 
+oc version client
+
+# Generate RHCOS Image
+echo "Generating RHCOS Image"
+mkdir -p /mnt/vm
+cd /mnt/vm
+
+RHCOS_ISO="rhcos-live.x86_64.iso"
+RHCOS_ISO_URL="https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/latest/$RHCOS_ISO"
+
+curl -LOv $RHCOS_ISO_URL
+
+echo "Install system dependencies"
+yum -y install qemu-img qemu-kvm libvirt libgcrypt-devel
+
+echo "Start libvirtd"
+systemctl start libvirtd
+
+echo "Create qcow2 disk files"
+qemu-img create -f qcow2 coreos-b.qcow2 30g
+qemu-img create -f qcow2 coreos-m.qcow2 30g
+qemu-img create -f qcow2 coreos-w.qcow2 30g
 
 echo "=== OpenShift LabAY Install Script Done $VERSION ==="
