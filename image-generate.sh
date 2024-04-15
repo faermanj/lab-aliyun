025fd1ed1b43468dc62a78f9a31696c572244bda
Julio Faerman <356476+faermanj@users.noreply.github.com>
2024-04-15 18:54:42 +0200

    wip

--- image-generate.sh
+++ image-generate.sh
@@ -1,25 +1,43 @@
+#!/bin/bash
+set -x
+export DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+
 # Generate RHCOS Image
 echo "Generating RHCOS Image"
-mkdir -p /mnt/vm
-cd /mnt/vm
-
+export RHCOS_TMP="$DIR/.vm"
 export RHCOS_ISO="rhcos-live.x86_64.iso"
+export RHCOS_PATH="$RHCOS_TMP/$RHCOS_ISO"
 export RHCOS_ISO_URL="https://mirror.openshift.com/pub/openshift-v4/dependencies/rhcos/latest/$RHCOS_ISO"
 
-curl -LOv $RHCOS_ISO_URL
+mkdir -p "$RHCOS_TMP"
+
+# check if iso path exists, download only if not
+if [ ! -f "$RHCOS_PATH" ]; then
+    curl -L -o "$RHCOS_PATH" "$RHCOS_ISO_URL"
+fi
 
 echo "Install system dependencies"
 yum -y install qemu-img qemu-kvm libvirt libgcrypt-devel
 
 echo "Start libvirtd"
-systemctl start libvirtd
+sudo systemctl start libvirtd
+# sleep 2
+# sudo systemctl status libvirtd
+
+echo "Clean existing files"
+rm -r *.qcow2
 
 echo "Create qcow2 disk files"
-qemu-img create -f qcow2 coreos-b.qcow2 30g
-qemu-img create -f qcow2 coreos-m.qcow2 30g
-qemu-img create -f qcow2 coreos-w.qcow2 30g
+qemu-img create -f qcow2 $RHCOS_TMP/coreos-b.qcow2 30g
+qemu-img create -f qcow2 $RHCOS_TMP/coreos-m.qcow2 30g
+qemu-img create -f qcow2 $RHCOS_TMP/coreos-w.qcow2 30g
 
-export RHCOS_ISO_PATH="/mnt/vm/$RHCOS_ISO"
-export QCOW_PATH="/mnt/vm/coreos-b.qcow2"
+export QCOW_PATH="$RHCOS_TMP/coreos-b.qcow2"
 export QCOW_NAME="coreos-b"
 envsubst < qcow-config.env.xml > qcow-config.$QCOW_NAME.xml
+
+echo "Creating bootstrap domain"
+virsh create qcow-config.$QCOW_NAME.xml
+
+
