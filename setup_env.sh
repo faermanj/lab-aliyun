ce998a993f899343f15d025adbac31d16961f9f2
Jianli Wei <jiwei@redhat.com>
2024-06-12 16:26:39 +0800

    QE updates

---
+++ setup_env.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+
+export AY_REGION="us-east-1"
+export AY_BASE_DOMAIN="alicloud-qe.devcluster.openshift.com"
+export AY_PREFIX="jiwei-0612b-ali-"
+#export AY_OCP_VERSION="4.16.0-rc.4"
+
+export SSH_KEY_PAIR_NAME="jiwei-0606a-ali-ssh-pub-key"
