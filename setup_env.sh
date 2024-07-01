e3ad02772d063b16f1f804522aaabc90f715804d
Jianli Wei <jiwei@redhat.com>
2024-07-01 18:45:52 +0800

    create SnatEntry for NatGateway

--- setup_env.sh
+++ setup_env.sh
@@ -2,7 +2,7 @@
 
 export AY_REGION="us-east-1"
 export AY_BASE_DOMAIN="alicloud-qe.devcluster.openshift.com"
-export AY_PREFIX="jiwei-0612b-ali-"
-#export AY_OCP_VERSION="4.16.0-rc.4"
+export AY_PREFIX="jiwei-0701a-ali-"
+export AY_OCP_VERSION="4.16.0"
 
-export SSH_KEY_PAIR_NAME="jiwei-0606a-ali-ssh-pub-key"
+export SSH_KEY_PAIR_NAME="jiwei-test-ssh-pub-key"
